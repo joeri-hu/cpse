013b623b9c1202cc6e4ebfb083b8c3f889417752
joeri-hu <joeri.j.kok@student.hu.nl>
2020-09-27 21:54:33 +0200

    fixed indentation

--- cpse-week1-joeri-hu/exercise-1.1/asciz.asm
+++ cpse-week1-joeri-hu/exercise-1.1/asciz.asm
@@ -3,11 +3,11 @@ print_asciz:
     push {r5, lr}
     mov r5, r0
 loop:
-	ldrb r0, [r5]
-	add r0, r0, #0
-	beq done
-	bl uart_put_char
-	add r5, r5, #1
-	b loop
+    ldrb r0, [r5]
+    add r0, r0, #0
+    beq done
+    bl uart_put_char
+    add r5, r5, #1
+    b loop
 done:
     pop {r5, pc}
