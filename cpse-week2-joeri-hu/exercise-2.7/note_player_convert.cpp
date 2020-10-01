1d1e3b46933053336c2443f9826d43d6d20f0a07
joeri-hu <joeri.j.kok@student.hu.nl>
2020-10-01 23:37:08 +0200

    completion seventh exercise

---
+++ cpse-week2-joeri-hu/exercise-2.7/note_player_convert.cpp
@@ -0,0 +1,36 @@
+#include "note_player_convert.hpp"
+#include <fstream>
+
+const std::string note_player_convert::extension{".cpp"};
+
+note_player_convert::note_player_convert(
+    std::string const& name
+):
+    name{name},
+    melody{name + extension}
+{
+    header();
+}
+
+note_player_convert::~note_player_convert() {
+    footer();
+}
+
+void note_player_convert::header() {
+    melody <<
+        "#include \"melody.hpp\"\n"
+        "\n"
+        "void melody::play(note_player& player) {\n";
+}
+
+void note_player_convert::footer() {
+    melody << "}\n";
+}
+
+void note_player_convert::play(note const& n) {
+    melody
+        << "    player.play({"
+        << n.frequency << ", "
+        << n.duration
+        << "});\n";
+}
