e828f202d57d69991d5693f3c3eae2435b05b38e
joeri-hu <joeri.j.kok@student.hu.nl>
2020-10-01 21:16:18 +0200

    completion fifth exercise

---
+++ cpse-week2-joeri-hu/exercise-2.5/melody.cpp
@@ -0,0 +1,39 @@
+#ifndef _melody_HPP
+#define _melody_HPP
+
+#include "melody.hpp"
+
+class melody : public melody {
+public:
+    void play(note_player& player) {
+        player.play({1318, 150000});
+        player.play({1174, 300000});
+        player.play({1046, 600000});
+        player.play({1318, 150000});
+        player.play({1568, 300000});
+        player.play({1760, 300000});
+        player.play({1568, 300000});
+        player.play({1568, 150000});
+        player.play({1568, 300000});
+        player.play({1318, 300000});
+        player.play({1174, 150000});
+        player.play({1046, 300000});
+        player.play({880, 150000});
+        player.play({784, 300000});
+        player.play({1318, 600000});
+        player.play({0, 600000});
+        player.play({1318, 300000});
+        player.play({1318, 150000});
+        player.play({1396, 300000});
+        player.play({1318, 300000});
+        player.play({1318, 300000});
+        player.play({1174, 300000});
+        player.play({0, 150000});
+        player.play({1318, 150000});
+        player.play({1174, 150000});
+        player.play({1174, 300000});
+        player.play({1046, 1200000});
+    }
+};
+
+#endif
